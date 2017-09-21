113220818a376e22b40f8186deb7313a216d0c7b
vilgefortzz <greghause123@gmail.com>
2017-09-21 20:38:02 +0200

    Binding contract view was improved

--- resources/views/contracts/binding-contract.blade.php
+++ resources/views/contracts/binding-contract.blade.php
@@ -18,12 +18,13 @@
                                         </h1>
                                         <div class="text text-center">
                                             <h1>Binding contract</h1>
-                                            <h1><img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-2.jpg"
+                                            <h1><img src="{{ asset($contract->user->avatar_dir. $contract->user->avatar) }}"
                                                      width="60" height="60" class="img-fluid rounded-circle"></h1>
                                             <h1><i class="fa fa-long-arrow-down"></i></h1>
                                             <h1><img src="{{ asset('images/contracts/signature.png') }}" width="130"></h1>
                                             <h1><i class="fa fa-long-arrow-down"></i></h1>
-                                            <h1><img src="{{ asset('images/clubs/'. $contract->club->logo) }}" width="60" height="60"></h1>
+                                            <h1><img src="{{ asset($contract->club->emblem_dir. $contract->club->emblem) }}"
+                                                     width="60" height="60"></h1>
                                         </div>
                                     </div>
                                 </div>
@@ -31,17 +32,22 @@
                                 <div class="col">
                                     <h1 class="font-bold font-italic">Contract details:</h1>
                                     <br>
-                                    <h3 class="font-italic"><i class="fa fa-calendar fa-fw"></i>Signed on: {{ $contract->date_and_time_of_signing }}</h3>
-                                    <h3 class="font-italic"><i class="fa fa-clock-o fa-fw"></i>Contract duration: {{ $contract->duration }}</h3>
-                                    <h3 class="font-italic"><i class="fa fa-calendar-minus-o fa-fw"></i>End of contract: {{ $contract->date_and_time_of_end }}</h3>
+                                    <h3 class="font-italic"><i class="fa fa-calendar fa-fw"></i>
+                                        Signed on: {{ $contract->date_and_time_of_signing }}</h3>
+                                    <h3 class="font-italic"><i class="fa fa-clock-o fa-fw"></i>
+                                        Contract duration: {{ $contract->duration }}</h3>
+                                    <h3 class="font-italic"><i class="fa fa-calendar-minus-o fa-fw"></i>
+                                        End of contract: {{ $contract->date_and_time_of_end }}</h3>
                                     <br><br>
                                     <h1 class="font-bold font-italic">Transfer:</h1>
                                     <br>
                                     <h3>
-                                        <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-2.jpg"
-                                             width="60" height="60" class="img-fluid rounded-circle"> {{ $contract->user->username }}
+                                        <img src="{{ asset($contract->user->avatar_dir. $contract->user->avatar) }}"
+                                             width="60" height="60" class="img-fluid rounded-circle">
+                                        {{ $contract->user->username }}
                                         <i class="fa fa-long-arrow-right"></i>
-                                        <img src="{{ asset('images/clubs/'. $contract->club->logo) }}" width="60" height="60"> {{ $contract->club->name }}
+                                        <img src="{{ asset($contract->club->emblem_dir. $contract->club->emblem) }}"
+                                             width="60" height="60"> {{ $contract->club->name }}
                                     </h3>
                                 </div>
                             </div>
